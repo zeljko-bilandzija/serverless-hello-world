d55b36065d1a1908f7e560aeb7690609de9e3205
Željko Bilandžija <cigara9393@hotmail.com>
2019-08-17 14:10:21 +0200

    Clean commit

--- handler.js
+++ handler.js
@@ -2,15 +2,10 @@
 
 module.exports.hello = async event => {
     const name = event.queryStringParameters && event.queryStringParameters.name;
-    const message = 'Hello ' + (name || 'World'); 
+    const message = `Hello ${name || 'World'}`;
+
     return {
         statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: message
-            },
-            null,
-            2
-        ),
+        body: JSON.stringify({ message }, null, 2),
     };
 };
